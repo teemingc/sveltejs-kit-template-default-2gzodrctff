00c80e0150868497c857a7f7c9dc62e17f8030dd
teemingc <chewteeming01@gmail.com>
2025-09-18 15:26:35 +0800

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
-  import { demo } from './demo.remote';
+	import { demo } from "./demo.remote";
 </script>
 
 <form {...demo}>
-	<input name={demo.field('name.first')} />
-	<input name={demo.field('name.last')} />
+	<input name={demo.field("name.first")} />
+
+	<input name={demo.field("name.last")} />
 	<button type="submit">Demo submit</button>
 </form>
 
-		{#if demo.issues?.name.last}
-			{#each createPost.issues.name.last as issue}
-				<p class="issue">{issue.message}</p>
-			{/each}
-		{/if}
+{#if demo.issues}
+	{#each Object.values(demo.issues) as field}
+		{#each field as issue}
+			<p>{issue.name}: {issue.message}</p>
+		{/each}
+	{/each}
+{/if}
